2c74eb2b12790118d9bd1bf39a2312720ba1c25e
Vineetha1312 <vineetha.vinny312@gmail.com>
2023-03-23 10:59:23 +0000

    nxtTrendz Authorization2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import Header from '../Header'
+
+const Cart = () => (
+  <div className="cart-container">
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="cart"
+      className="cart-image"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import Header from '../Header'
+
+const Products = () => (
+  <div className="products-container">
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="product-image"
+    />
+  </div>
+)
+
+export default Products
